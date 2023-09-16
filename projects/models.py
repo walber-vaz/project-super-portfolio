f170269dda57719c764c5fbfebb960a4ecdc761e
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-09-16 12:07:56 -0300

    initial certificate

--- projects/models.py
+++ projects/models.py
@@ -1,11 +1,13 @@
-from django.core.validators import MaxLengthValidator
+from django.core.validators import MaxLengthValidator, URLValidator
 from django.db import models
 
 
 class Profile(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False)
-    github = models.URLField(null=False, blank=False)
-    linkedin = models.URLField(null=False, blank=False)
+    name = models.CharField(
+        max_length=100, validators=[MaxLengthValidator(limit_value=100)]
+    )
+    github = models.URLField(validators=[URLValidator()])
+    linkedin = models.URLField(validators=[URLValidator()])
     bio = models.TextField(
         null=False,
         blank=False,
@@ -17,15 +19,21 @@ class Profile(models.Model):
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False)
+    name = models.CharField(
+        max_length=50, validators=[MaxLengthValidator(limit_value=50)]
+    )
     description = models.TextField(
         null=False,
         blank=False,
         validators=[MaxLengthValidator(limit_value=500)],
     )
-    github_url = models.URLField(null=False, blank=False)
-    keyword = models.CharField(max_length=50, null=False, blank=False)
-    key_skill = models.CharField(max_length=50, null=False, blank=False)
+    github_url = models.URLField(validators=[URLValidator()])
+    keyword = models.CharField(
+        max_length=50, validators=[MaxLengthValidator(limit_value=50)]
+    )
+    key_skill = models.CharField(
+        max_length=50, validators=[MaxLengthValidator(limit_value=50)]
+    )
     profile = models.ForeignKey(
         Profile, on_delete=models.CASCADE, related_name='projects'
     )
@@ -35,15 +43,19 @@ class Project(models.Model):
 
 
 class CertifyingInstitution(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False)
-    url = models.URLField(null=False, blank=False)
+    name = models.CharField(
+        max_length=100, validators=[MaxLengthValidator(limit_value=100)]
+    )
+    url = models.URLField(validators=[URLValidator()])
 
     def __str__(self):
         return self.name
 
 
 class Certificate(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False)
+    name = models.CharField(
+        max_length=100, validators=[MaxLengthValidator(limit_value=100)]
+    )
     certifying_institution = models.ForeignKey(
         CertifyingInstitution, on_delete=models.CASCADE
     )
--- projects/serializers.py
+++ projects/serializers.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from rest_framework import serializers
 
 from .models import Certificate, CertifyingInstitution, Profile, Project
@@ -28,17 +30,26 @@ class CertifyingInstitutionSerializer(serializers.ModelSerializer):
         model = CertifyingInstitution
         fields = '__all__'
 
+    def to_representation(self, instance):
+        data = OrderedDict()
+        data['id'] = instance.id
+        data['name'] = instance.name
+        data['url'] = instance.url
+        data['certificates'] = CertificateSerializer(
+            instance.certificate_set.all(), many=True
+        ).data
+
+        return data
+
     def create(self, validated_data):
-        certificates_data = validated_data.pop("certificates", [])
+        certificates_data = validated_data.pop('certificates')
         certifying_institution = CertifyingInstitution.objects.create(
             **validated_data
         )
-        certificates = [
-            Certificate(
+        for certificate_data in certificates_data:
+            Certificate.objects.create(
                 certifying_institution=certifying_institution,
                 **certificate_data,
             )
-            for certificate_data in certificates_data
-        ]
-        Certificate.objects.bulk_create(certificates)
+
         return certifying_institution
